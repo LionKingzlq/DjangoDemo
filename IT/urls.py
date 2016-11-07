b6ed3cb0427fc3b30fe2f4b569908246cfec5690
LionKingzlq <LionKing.zlq@gmail.com>
2016-11-08 01:02:04 +0800

    something 很混乱

--- IT/urls.py
+++ IT/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from IT import views as DemoViews
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^$', DemoViews.index),
-    url(r'^json$', DemoViews.jsonTest)
+    url(r'^json$', DemoViews.jsonTest),
+    url(r'^admin', TemplateView.as_view(template_name='IT/Admin/index.html'))
 ]
