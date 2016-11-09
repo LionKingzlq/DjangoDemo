c5c19b7957c9e7ed96af5658830b0710149bcd82
LionKingzlq <LionKing.zlq@gmail.com>
2016-11-10 01:21:32 +0800

    项目文件结构和名称重新调整

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,6 +4,9 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^$', blogViews.index),
-    url(r'^json$', blogViews.jsonTest),
+    url(r'^get', blogViews.get),
+    url(r'^add', blogViews.add),
+    url(r'^update', blogViews.add),
+    url(r'^detele', blogViews.add),
     url(r'^admin', TemplateView.as_view(template_name='blog/admin/index.html'))
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -20,13 +20,31 @@ def index(request):
     return render(request, "blog/index.html", context)
 
 
-def test(request):
-    return render(request, "test.html")
-
-
-def jsonTest(request):
+def add(request):
     response_data = {}
     response_data['result'] = 'failed'
     response_data['message'] = 'You messed up'
     # json_adta = serializers.serialize('json',response_data)
     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def delete(request):
+    response_data = {}
+    response_data['result'] = 'failed'
+    response_data['message'] = 'You messed up'
+    # json_adta = serializers.serialize('json',response_data)
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def update(request):
+    response_data = {}
+    response_data['result'] = 'failed'
+    response_data['message'] = 'You messed up'
+    # json_adta = serializers.serialize('json',response_data)
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def get(request):
+    return render(request, "test.html")
+
+
